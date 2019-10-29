668590fa682f9f0a43231c3e803e81d113052cc4
Edvan Ronchi <edvan@minertecnologia.com>
2019-10-29 14:30:33 -0300

    Comunicaçao front-back

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -6,54 +6,67 @@ import './style.css'
 export default function LoginRegister() {
     const [ show, setShow] = useState(true);
     const [ namebtn, setNamebtn] = useState('Login');
+    
     const [ login, setLogin] = useState({
         email: '',
         senha: ''
     })
+
     const [ cadastro, setCadastro] = useState({
         nome: '',
         email: '',
         senha: '',
         senha2: ''
     })
-    
-    
+
+    const updateLogin = (field, value) => {
+        setLogin({ ...login, [field]: value })
+    };
+
+    const updateCadastro = (field, value) => {
+        setCadastro({ ...cadastro, [field]: value })
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault()
         
         if(show === true){
-            console.log(login.email)
-            console.log(login.senha)
-            
-            /* async function axios(){
+            async function axios(){
                 await api.post('auth/login', {
-                    
+                    email: login.email,
+                    senha: login.senha
+
                 }).then((response) => {
                     localStorage.setItem('token', response.data.token)
-                    
+                    console.log("Deu Bom, logado")
                     
                 }).catch((err) => {
                     console.log("Deu ruim")
                 })
-            } */
-    
+            }
+            
+            axios()
            
         }else{
-            console.log(cadastro.nome, cadastro.email, cadastro.senha, cadastro.senha2)
+            if(cadastro.senha !== cadastro.senha2)
+                return console.log("Senha não gemeas")
 
-
-            /* async function axios(){
+            async function axios(){
                 await api.post('auth/register', {
-                    
+                    nome:  cadastro.nome,
+                    email: cadastro.email,
+                    senha: cadastro.senha
+
                 }).then((response) => {
                     localStorage.setItem('token', response.data.token)
-                    console.log(response.data.token);
-                    
+                    console.log("Deu Bom, registrado");
                     
                 }).catch((err) => {
                     console.log("Deu ruim")
                 })
-            } */
+            }
+
+            axios()
         }
     }
 
@@ -70,32 +83,32 @@ export default function LoginRegister() {
                     {show && (
                         <div>
                             <div className="input-group mb-3">
-                                <input type="text" className="form-control" placeholder="E-mail" onChange={(e) => setLogin({email: e.target.value})}/>
+                                <input type="text" className="form-control" placeholder="E-mail" onChange={ event => updateLogin("email", event.target.value)}/>
                                 <div className="input-group-append">
                                     <span className="input-group-text">@react.com</span>
                                 </div>
                             </div>
                             <div className="input-group mb-3">
-                                <input type="password" className="form-control" placeholder="Senha" onChange={(e) => setLogin({senha: e.target.value})}/>
+                                <input type="password" className="form-control" placeholder="Senha" onChange={event => updateLogin("senha", event.target.value)}/>
                             </div>
                         </div>           
                     )}
                     {!show && (
                         <div>
                             <div className="input-group mb-3">
-                                <input type="text" className="form-control" placeholder="Nome" onChange={(e) => setCadastro({nome: e.target.value})}/>
+                                <input type="text" className="form-control" placeholder="Nome" onChange={event => updateCadastro("nome", event.target.value)}/>
                             </div>
                             <div className="input-group mb-3">
-                                <input type="text" className="form-control" placeholder="E-mail" onChange={(e) => setCadastro({email: e.target.value})}/>
+                                <input type="text" className="form-control" placeholder="E-mail" onChange={event => updateCadastro("email", event.target.value)}/>
                                 <div className="input-group-append">
                                     <span className="input-group-text">@react.com</span>
                                 </div>
                             </div>
                             <div className="input-group mb-3">
-                                <input type="password" className="form-control" placeholder="Senha" onChange={(e) => setCadastro({senha: e.target.value})}/>
+                                <input type="password" className="form-control" placeholder="Senha" onChange={event => updateCadastro("senha", event.target.value)}/>
                             </div>
                             <div className="input-group mb-3">
-                                <input type="password" className="form-control" placeholder="Confirmar Senha" onChange={(e) => setCadastro({senha2: e.target.value})}/>
+                                <input type="password" className="form-control" placeholder="Confirmar Senha" onChange={event => updateCadastro("senha2", event.target.value)}/>
                             </div>
                         </div>
                     )}
